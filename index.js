9fdf3a4137a82cc297568562549ec18bf6b71629
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-02-28 20:34:49 +0300

    my secconddom

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
 }
+
+// Function that binds the event listener to the input#button element
+function addingEventListener() {
+    const input = document.getElementById('button'); // Grab the input element
+  
+    // Ensure that the element exists before adding the event listener
+    if (input) {
+      input.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
+    }
+  }
+  
+  // Call the function to add the event listener
+  addingEventListener();
+  
